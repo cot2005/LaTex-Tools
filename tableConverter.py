e4959bc53be5dd681a50ae98d4e127a81673e4db
Colin Tang <53540424+cot2005@users.noreply.github.com>
2021-03-30 15:51:49 -0700

    Create tableConverter.py

---
+++ tableConverter.py
@@ -0,0 +1,33 @@
+"""
+@author: Colin
+
+Simple function to convert a table to a standard LaTex table format
+"""
+
+def table2Latex(csvfile, header = True, sep = ","):
+     with open(csvfile, "r") as csvtable:
+        with open((csvfile[0:-4] + "_LaTex.txt"),"w") as latex:
+            latex.write("\hline\n")
+            if (header == True):
+                line = csvtable.readline()
+                l = line.strip().split(sep)
+                latexLine = " & ".join(l) + "\\\\ [0.5ex]\n"
+                latexLine = LaTexSyntax(latexLine)
+                latex.write(latexLine)
+                latex.write("\hline\hline\n")            
+            for line in csvtable:   #loop through all the lines starting at line 2
+                l = line.strip().split(sep)
+                latexLine = " & ".join(l) + "\\\\\n"
+                latexLine = LaTexSyntax(latexLine)
+                latex.write(latexLine)
+            latex.write("\hline")
+
+"""
+function to fix common latex syntax errors in text conversion. Cannot 
+be used to fix incomplete latex documents. currently only fixes % signs.
+Will update to include all common latex symbols.
+"""
+def LaTexSyntax(inputstring):
+    inputstring = inputstring.replace("%", "\%")
+    inputstring = inputstring.replace("#", "\#")
+    return(inputstring)
